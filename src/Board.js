2303bca9c4ed5d2e7e58bbe8d3ca2d4e323fbf9a
Jeof Petty <jeofpetty@icloud.com>
2023-03-26 03:00:08 -0400

    finshed board, and most logic.. to fix flipping lights

---
+++ src/Board.js
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import Cell from "./Cell";
+import "./Board.css";
+
+/** Game board of Lights out.
+ *
+ * Properties:
+ *
+ * - nrows: number of rows of board
+ * - ncols: number of cols of board
+ * - chanceLightStartsOn: float, chance any cell is lit at start of game
+ *
+ * State:
+ *
+ * - board: array-of-arrays of true/false
+ *
+ *    For this board:
+ *       .  .  .
+ *       O  O  .     (where . is off, and O is on)
+ *       .  .  .
+ *
+ *    This would be: [[f, f, f], [t, t, f], [f, f, f]]
+ *
+ *  This should render an HTML table of individual <Cell /> components.
+ *
+ *  This doesn't handle any clicks --- clicks are on individual cells
+ *
+ **/
+
+function Board({ nrows, ncols, chanceLightStartsOn }) {
+  const [board, setBoard] = useState(createBoard());
+
+  /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
+  function createBoard() {
+    let initialBoard = [];
+    // TODO: create array-of-arrays of true/false values
+    for (let y = 0; y < nrows; y++) {
+      initialBoard.push([]);
+      for (let x = 0; x < ncols; x++) {
+        initialBoard[y].push(Math.random() < chanceLightStartsOn);
+      }
+    }
+    return initialBoard;
+  }
+
+  function hasWon() {
+    // TODO: check the board in state to determine whether the player has won.
+    return board.filter(row => row.every(cell => cell === false))
+  }
+
+  function flipCellsAround(coord) {
+    setBoard(oldBoard => {
+      const [y, x] = coord.split("-").map(Number);
+
+      const flipCell = (y, x, boardCopy) => {
+        // if this coord is actually on board, flip it
+
+        if (x >= 0 && x < ncols && y >= 0 && y < nrows) {
+          boardCopy[y][x] = !boardCopy[y][x];
+        }
+      };
+
+      // TODO: Make a (deep) copy of the oldBoard
+      const newBoard = JSON.parse(JSON.stringify(oldBoard));
+      // TODO: in the copy, flip this cell and the cells around it
+      flipCell(y, x, newBoard);
+      // TODO: return the copy
+      return newBoard;
+    });
+  }
+
+  // if the game is won, just show a winning msg & render nothing else
+
+  // TODO
+
+  // make table board
+
+  // TODO
+  return (
+    <div className="Board">
+      <table>
+        <tbody>
+          {board.map((row, y) => (
+            <tr>
+              {row.map((cell, x) => (
+                <Cell
+                  key={`${y}-${x}`}
+                  isLit={cell}
+                  flipCellsAroundMe={() => flipCellsAround(`${y}-${x}`)}
+                />
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Board;
